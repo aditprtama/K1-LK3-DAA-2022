99f5f78a56e172f6f18f50954680e7dcbd4329e8
aditprtama <73066004+aditprtama@users.noreply.github.com>
2022-05-24 20:59:36 +0700

    Create nomor1.c

---
+++ nomor1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcnt.h>
+
+int main(){
+    int fd, size;
+    char *c = (char *) calloc(100, sizeof (char));
+
+    fd = open("bab9.txt", O_RDONLY);
+    if (fd < 0){
+        perror("Error");
+        exit(1);
+    }
+
+    size = read(fd, c, 20);
+    c[size]= '/0';
+    print("Isi file tersebut ialah: %s", c);
+}
